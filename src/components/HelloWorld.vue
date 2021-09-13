617b1e127ff6d8a5ec65530f3092946f4c6d3a39
Moinul Moin <moinulmoin101@gmail.com>
2021-09-13 16:24:58 +0600

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script>
+	export default {
+		name: 'HelloWorld',
+		props: ['msg'],
+	};
+</script>
+
+<template>
+	<h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+	a {
+		color: #42b983;
+	}
+</style>
